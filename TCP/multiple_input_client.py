baf0094591888febeae8db02af3483eec994c59a
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2018-02-28 17:11:52 +0700

    TCP

---
+++ TCP/multiple_input_client.py
@@ -0,0 +1,28 @@
+#import socket
+import socket
+
+#inisiasi objek
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#kirim req koneksi ke server
+tcp_sock.connect(("127.0.0.1", 6667))
+
+#kirim data
+data = "selamat pagi"
+tcp_sock.send(data.encode('utf-8'))
+
+#baca data yg dikirim balik oelh serer
+data = tcp_sock.recv(100)
+data = data.decode('utf-8')
+print(data)
+
+#tcp_sock.close
+
+#kirim data
+data = "selamat pagi"
+tcp_sock.send(data.encode('utf-8'))
+
+#baca data yg dikirim balik oelh serer
+data = tcp_sock.recv(100)
+data = data.decode('utf-8')
+print(data)
---
+++ TCP/simple_tcp_server.py
@@ -0,0 +1,30 @@
+#import socket
+import socket
+
+#inisiasi socket TCP/IPv4
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind ke address dan port tertentu
+tcp_sock.bind(('0.0.0.0', 6667))
+
+#listen sebanyak 100
+tcp_sock.listen(100)
+
+while True:
+    #terima req keneksi
+    conn, client_address = tcp_sock.accept()
+    
+    #terima string dari client
+    #parameter rcv besaran atau ukuran data yg dibaca
+    #Stream block
+    data = conn.recv(100)
+    data = data.decode('utf-8')[::-1]
+
+    #tambah data
+    data = 'OK ' + data
+    
+    #kirim balik ke client
+    conn.send(data.encode('utf-8'))
+
+    #tutup koneksi
+    #conn.close()
